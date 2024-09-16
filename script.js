5c747737a4ec1a594e980ed263c68cc6d93cb412
huy13214 <manesgopro.com@gmail.com>
2024-09-16 23:41:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function profileclick (){
+    let about = document.getElementById("huyabout").innerHTML="Xin chào! Tôi là Đ Huy, một Full Stack Developer với niềm đam mê sâu sắc đối với công nghệ và phát triển phần mềm. Với kinh nghiệm phong phú trong việc xây dựng các ứng dụng web và di động từ đầu đến cuối, tôi tự hào là người có khả năng làm việc hiệu quả trên cả front-end lẫn back-end.";
+}
+function profileclick2 (){
+    let about2 = document.getElementById("nghiabout").innerHTML="Xin chào! Tôi là Gia Nghi, một Full Stack Developer với niềm đam mê sâu sắc đối với công nghệ và phát triển phần mềm. Với kinh nghiệm phong phú trong việc xây dựng các ứng dụng web và di động từ đầu đến cuối, tôi tự hào là người có khả năng làm việc hiệu quả trên cả front-end lẫn back-end.";
+}
